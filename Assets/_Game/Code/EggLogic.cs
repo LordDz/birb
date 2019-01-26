1992493cff3a7aa351ba4356ebf387d5d785ec0f
oscfri <oscar.c.friberg@gmail.com>
2019-01-26 14:57:24 +0100

    Egg twiggles

--- Assets/_Game/Code/EggLogic.cs
+++ Assets/_Game/Code/EggLogic.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class EggLogic : MonoBehaviour
 {
+    public float timeToHatch = 10.0f;
+    public Transform eggTransform;
     public ParentToHidables eggHidable;
     public ParentToHidables babyHidable;
 
-    public float timeToHatch = 5.0f;
+    private float twiggleTimer = 0.0f;
+
+    private Vector3 eggOriginPosition;
 
     void Start()
     {
+        eggOriginPosition = eggTransform.position;
     }
 
     public void UpdateHatchTimer()
@@ -28,4 +33,17 @@ public class EggLogic : MonoBehaviour
         return timeToHatch < 0.0f;
     }
 
+    void Update()
+    {
+        if (Random.value < 1 / (timeToHatch + 1e-6) * Time.deltaTime * 5.0f)
+        {
+            twiggleTimer = 0.1f;
+        }
+        if (twiggleTimer > 0.0f)
+        {
+            float shift = (Random.value - 0.5f) * 0.1f;
+            eggTransform.position = eggOriginPosition + new Vector3(shift, 0.0f, 0.0f);
+        }
+        twiggleTimer -= Time.deltaTime;
+    }
 }
