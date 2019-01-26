9a2705842f1799d1914452a4f26c5bf8db8f7780
oscfri <oscar.c.friberg@gmail.com>
2019-01-26 11:43:57 +0100

    Added jump transition

---
+++ Assets/_Game/Code/Hidable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hidable : MonoBehaviour
+{
+    private bool isHiding;
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.localPosition;
+    }
+
+    public void Hide()
+    {
+        transform.localPosition = new Vector3(0.0f, -10.0f, 0.0f);
+        isHiding = true;
+    }
+
+    public void Unhide()
+    {
+        transform.localPosition = startPosition;
+        isHiding = false;
+    }
+
+    public bool IsHiding()
+    {
+        return isHiding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Game/Code/TransitionMovement.cs
+++ Assets/_Game/Code/TransitionMovement.cs
@@ -6,22 +6,17 @@ public class TransitionMovement : MonoBehaviour
 {
     enum State
     {
-        NONE,
+        FINISHED,
         FORWARD,
         BACKWARD
     };
 
+    public float transitionSpeed = 5.0f;
     public Transform positionStart;
     public Transform positionEnd;
 
-    private State state = State.NONE;
+    private State state = State.FINISHED;
     private float transitionAlpha = 0.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     public void StartTransitionForward()
     {
@@ -35,10 +30,30 @@ public class TransitionMovement : MonoBehaviour
         transitionAlpha = 1.0f;
     }
 
+    public bool IsFinished()
+    {
+        return state == State.FINISHED;
+    }
+
+    Vector3 QuadraticBezier(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        return (1 - t) * ((1 - t) * p0 + t * p1) + t * ((1 - t) * p1 + t * p2);
+    }
+
+    void Transition()
+    {
+        Vector3 topPosition = (positionEnd.position + positionStart.position) / 2 + new Vector3(0.0f, 2.0f, 0.0f);
+        transform.position = QuadraticBezier(
+                positionStart.position,
+                topPosition,
+                positionEnd.position,
+                transitionAlpha);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (state == State.NONE)
+        if (state == State.FINISHED)
         {
             transform.position = positionStart.position - new Vector3(0.0f, -10.0f, 0.0f);
         }
@@ -46,22 +61,21 @@ public class TransitionMovement : MonoBehaviour
         {
             if (state == State.FORWARD)
             {
-                transitionAlpha += 0.1f * Time.deltaTime;
+                transitionAlpha += transitionSpeed * Time.deltaTime;
                 if (transitionAlpha > 1.0f)
                 {
-                    state = State.NONE;
+                    state = State.FINISHED;
                 }
             }
             else if (state == State.BACKWARD)
             {
-                transitionAlpha -= 0.1f * Time.deltaTime;
+                transitionAlpha -= transitionSpeed * Time.deltaTime;
                 if (transitionAlpha < 0.0f)
                 {
-                    state = State.NONE;
+                    state = State.FINISHED;
                 }
             }
-            Debug.Log(transitionAlpha);
-            transform.position = Vector3.Lerp(positionStart.position, positionEnd.position, transitionAlpha);
+            Transition();
         }
     }
 }
