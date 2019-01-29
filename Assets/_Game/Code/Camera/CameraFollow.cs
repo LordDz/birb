21f5fb39c562b5961e06edb0ab98c4da96c91300
oscfri <oscar.c.friberg@gmail.com>
2019-01-29 21:00:13 +0100

    Fixed game over bug

---
+++ Assets/_Game/Code/Camera/CameraFollow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform objectToFollow;
+
+    private Vector3 originalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    void FollowObject()
+    {
+        float followX = objectToFollow.transform.position.x;
+        if (followX > transform.position.x + 2.5f)
+        {
+            transform.position = new Vector3(
+                    followX - 2.5f,
+                    originalPosition.y,
+                    originalPosition.z);
+        }
+        else if (followX < transform.position.x - 2.5f)
+        {
+            transform.position = new Vector3(
+                    followX + 2.5f,
+                    originalPosition.y,
+                    originalPosition.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (objectToFollow.gameObject.activeSelf)
+        {
+            FollowObject();
+        }
+        else
+        {
+            Vector3 direction = originalPosition - transform.position;
+            if (direction.magnitude > 1.0f)
+            {
+                transform.position += direction / direction.magnitude;
+            }
+            else
+            {
+                transform.position = originalPosition;
+            }
+        }
+    }
+}
