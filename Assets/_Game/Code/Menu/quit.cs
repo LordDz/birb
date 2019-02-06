277c5ea970e099fd1018c9785d3b1218b4ae2dab
steddenhub <stedden@gmail.com>
2019-02-06 23:20:28 +0100

    Quit on escape

---
+++ Assets/_Game/Code/Menu/quit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    void Update()
+    {
+		if(Input.GetKeyUp("escape"))
+		{
+			Debug.Log("quitting");
+			Application.Quit();
+		}
+    }
+}
